9a218a76704ec82705c471132b9020d55b022515
oubeichen <oubeichen@gmail.com>
2014-12-02 00:04:10 +0800

    add condition fragment

--- src/com/oubeichen/weather/AddAlarmActivity.java
+++ src/com/oubeichen/weather/AddAlarmActivity.java
@@ -1,14 +1,32 @@
 package com.oubeichen.weather;
 
-import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
-public class AddAlarmActivity extends Activity {
+import com.oubeichen.weather.ConditionFragment.OnFragmentInteractionListener;
+
+public class AddAlarmActivity extends FragmentActivity implements
+        OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
     }
+    
+    public void addConditionClick(View view) {
+        FragmentTransaction transaction = getSupportFragmentManager()
+                .beginTransaction();
+        transaction.add(R.id.condition_list, ConditionFragment.newInstance("aaa", "bbb"));
+        transaction.commit();
+    }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // TODO Auto-generated method stub
+        
+    }
 }
